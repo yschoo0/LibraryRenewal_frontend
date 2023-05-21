b14a0d53e11cc8fbad64b9e3ff9626d8d96f2d4f
yschoo0 <yyssms@daum.net>
2023-05-21 20:24:27 +0900

    Books

--- library-renewal/src/screens/Books.js
+++ library-renewal/src/screens/Books.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import './Books.css';
+
 
 const Books = () => {
-  return <div>Books</div>;
+  return(
+    <div className='box'>
+      <header className='home-header'>
+      <h1>
+        <span></span> 신착/인기도서 <span></span>
+      </h1>
+    </header>
+    <div className="new">
+        <h4>신착도서</h4>
+        <ul>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="thin"><p>책 제목</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="thin"><p>책 제목</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+        </ul>
+    </div>
+    <div className="best">
+        <h4>인기도서</h4>
+        <ul>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="thin"><p>책 제목</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="thin"><p>책 제목</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+            <li class="book"><p>책 제목 및 저자</p></li>
+ 
+        </ul>
+    </div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+    <div className="space-for-nav" style={{ height :"4rem" }}></div>
+</div>
+
+  )
 };
+
 
 export default Books;
